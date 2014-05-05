073864db6f7d8e4039bcf5bbd4d883ef9af0dff5
Tom Shea <tom@shea.at>
2014-05-05 16:45:16 -0700

    Initial framework

---
+++ Madscience.java
@@ -0,0 +1,15 @@
+package madscience.madscience;
+
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Madscience.MODID, version = Madscience.VERSION)
+
+public class Madscience {
+    public static final String MODID = "madscience";
+    public static final String VERSION = "0.1";
+
+}

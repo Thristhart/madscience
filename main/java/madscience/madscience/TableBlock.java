43a4fc82c6e06be99c7d2e9fdced7c95698298bc
Tom Shea <tom@shea.at>
2014-05-08 18:18:11 -0700

    Begin implementing table specifics

--- main/java/madscience/madscience/TableBlock.java
+++ main/java/madscience/madscience/TableBlock.java
@@ -6,5 +6,8 @@ import net.minecraft.block.material.Material;
 public class TableBlock extends Block {
 	public TableBlock (Material material) {
 		super(material);
+		this.setHardness(0.5f);
+		this.setStepSound(Block.soundTypeMetal);
+		this.setUnlocalizedName("madscienceTable");
 	}
 }

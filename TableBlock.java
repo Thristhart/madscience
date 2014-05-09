eaa1cd503e5ca349e374701381471ffead6bcebc
Tom Shea <tom@shea.at>
2014-05-08 17:47:55 -0700

    TableBlock class

---
+++ TableBlock.java
@@ -0,0 +1,10 @@
+package madscience.madscience;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class TableBlock extends Block {
+	public TableBlock (Material material) {
+		super(material);
+	}
+}
